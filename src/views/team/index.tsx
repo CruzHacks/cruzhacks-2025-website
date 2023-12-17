a2300170c41debea7fa9a4e81dd8a049411b946c
Zack Traczyk <me@zacktraczyk.com>
2023-12-17 06:42:24 -0800

    Add Organizers to Team Page (#34)
    Co-authored-by: Zack Traczyk <20495250+xxzbuckxx@users.noreply.github.com>

--- src/views/team/index.tsx
+++ src/views/team/index.tsx
@@ -1,21 +1,52 @@
 import React from "react"
+import ScrollToTop from "../../components/scrollControl/ScrollToTop"
 import Navbar from "../../components/Navbar"
 import Footer from "../../components/Footer"
+import { Organizer, advisors, boardOfDirectors, organizers } from "./data"
 
 import Binary from "../../assets/Binary.jpg"
 import Lake from "../../assets/illustrations/Lake.svg"
-import ScrollToTop from "../../components/scrollControl/ScrollToTop"
+import LinkedIn from "../../assets/icons/LinkedIn.svg"
+import CruzHacksLogo from "../../assets/logos/CruzHacks.svg"
+import { AsyncImage } from "loadable-image"
+import Star from "../../components/Star"
 
-const boardOfDirectors = [
-  "Doug Erickson",
-  "Nathan Westrup",
-  "Amanda Rotella",
-  "Suz Howells",
-  "Mansi Saini",
-  "Nada Miljkovic",
-]
+const TeamMember = ({ name, role, image, linkedIn }: Organizer) => {
+  if (!image || image == "") {
+    image = CruzHacksLogo
+  }
 
-const advisors = ["Alexi Sevastopoulos", "Dr. Ebonée Williams", "Mark Adams"]
+  return (
+    <div className='flex flex-col items-center gap-3'>
+      <AsyncImage
+        src={image}
+        alt={name + " profile picture"}
+        style={{
+          height: "10rem",
+          width: "10rem",
+          borderRadius: "9999px",
+          objectFit: "cover",
+          border: "4px solid rgb(211 218 244 / 0.2)",
+        }}
+        loader={<div className='h-8 w-8 bg-blue-imperial/50' />}
+      />
+      <h3 className='text-center font-title text-lg uppercase md:text-lg'>
+        {name}
+      </h3>
+      <p className='text-center font-subtext'>{role}</p>
+      {linkedIn && linkedIn != "" && (
+        <a
+          href={linkedIn}
+          target='_blank'
+          rel='noopener noreferrer'
+          className='text-center font-subtext'
+        >
+          <img src={LinkedIn} alt='' className='h-8 w-8' />
+        </a>
+      )}
+    </div>
+  )
+}
 
 const Team = () => {
   return (
@@ -31,11 +62,22 @@ const Team = () => {
       >
         <Navbar />
         <div className='h-10' />
-        <div className='flex flex-col items-center gap-6'>
+
+        <Star
+          i={2}
+          className='absolute left-20 mt-36 h-4 md:left-40 md:mt-40 md:h-5 lg:left-80 lg:h-7'
+        />
+
+        <Star
+          i={3}
+          className='absolute left-12 mt-44 h-6 md:left-20 md:mt-52 md:h-7 lg:left-60 lg:h-10'
+        />
+
+        <div className='flex flex-col items-center gap-6 py-10 md:py-20'>
           <h2 className='text-center font-title text-3xl uppercase md:text-5xl'>
             Our History
           </h2>
-          <p className='max-w-2xl text-center font-subtext'>
+          <p className='max-w-2xl text-center font-subtext text-lg'>
             CruzHacks was founded in 2013 as Hack UCSC by Mark Adams, Brent
             Haddad, and Doug Erickson. In 2018, Hack UCSC was rebranded as
             CruzHacks, and became a student-led non-profit hackathon. Throughout
@@ -45,7 +87,24 @@ const Team = () => {
           </p>
         </div>
 
-        <div className='flex flex-col items-center gap-10 md:flex-row md:items-start'>
+        <div className='relative'>
+          <Star
+            i={4}
+            className='absolute right-12 -mt-20 h-3 md:right-20 md:h-5'
+          />
+
+          <h2 className='pb-20 text-center font-title text-3xl uppercase md:text-5xl'>
+            2024 Organizing Team
+          </h2>
+
+          <div className='grid grid-cols-1 gap-5 gap-y-20 md:grid-cols-3'>
+            {organizers.map(organizer => {
+              return <TeamMember key={organizer.name} {...organizer} />
+            })}
+          </div>
+        </div>
+
+        <div className='flex flex-col items-center gap-10 py-10 md:flex-row md:items-start md:py-20'>
           <div className='flex max-w-sm flex-1 flex-col items-center gap-6'>
             <h3 className='text-center font-title text-2xl uppercase md:text-4xl'>
               Board of directors
