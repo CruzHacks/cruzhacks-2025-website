3a8b290b87f4c0119876d39cbdf77d4e17ee411a
Kartikeya <kartikeyaku@gmail.com>
2024-11-19 17:02:28 -0800

    Removed Text SVGs from src/assets/ocean

--- src/views/(static)/SponsorshipPage/3 - Ocean/OceanComponents/Interested.tsx
+++ src/views/(static)/SponsorshipPage/3 - Ocean/OceanComponents/Interested.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Interested = () => {
   return (
-   <p style={{width: "500px", fontFamily: "Monda", fontSize: "26px",}}>
+   <p style={{width: "500px", fontFamily: "Monda", fontSize: "26px", marginLeft: "20px"}}>
     If you are interested in sponsoring our event, please email us at sponsor@cruzhacks.com and
     we will be ready to answer any questions and move forward with the onboarding process.
    </p>
--- src/views/(static)/SponsorshipPage/3 - Ocean/OceanComponents/NextSteps.tsx
+++ src/views/(static)/SponsorshipPage/3 - Ocean/OceanComponents/NextSteps.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NextSteps = ({ className }: { className?: string }) => {
   return (
-      <div style={{fontSize: "96px", marginTop: "250px", fontFamily: "Madimi One", color: "white", marginBottom: "30px"}}>
+      <div style={{fontSize: "76px", marginTop: "250px", fontFamily: "Madimi One", color: "white", marginBottom: "60px", marginLeft: "20px"}}>
         NEXT STEPS:
       </div>
   );
