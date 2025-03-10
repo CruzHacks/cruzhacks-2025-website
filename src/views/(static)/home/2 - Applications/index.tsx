7f8f74055321d9adb1b61327dddbbcde5a22262a
Sophie Han <32656696+sophiiehan@users.noreply.github.com>
2025-03-10 13:55:37 -0700

    Updated home page announcement

--- src/views/(static)/home/2 - Applications/index.tsx
+++ src/views/(static)/home/2 - Applications/index.tsx
@@ -18,34 +18,37 @@ const ApplicationBlurb = ({ id }: { id?: string }) => {
         <div className='justify-between items-center'>
           <div className="flex justify-between align-middle w-screen">
             <div className="w-full pt-10 mx-20 items-center">
+
+              {/* Announcement */}
               <div className=" flex flex-col gap-2 md:px-16 md:py-8 px-8 py-4 border-teal_blue border-4 text-lg md:text-2xl text-center text-sponsor_dark_orange  md:mx-40 mb-10 md:mb-20">
                 <div className="text-3xl"><b>Great News!</b></div>
                 <div className="text-xl"> We have finalized a new date and venue for CruzHacks 2025! CruzHacks 2025 will take place <b>April 11th-13th at the Civic Auditorium Center!</b></div>
                 <div className="text-xl "> <b>Note:</b> Our new application deadline will be <b>Saturday, March 8th @ 11:59PM.</b> Get your application in before then!</div>
               </div>
+
+
+
               <h1 className='font-heading text-5xl uppercase text-center md:text-center md:text-6xl text-sponsor_dark_orange'>
-                Applications Are Open NOW!
+                Applications Are Now Closed!
               </h1>
 
               <div className='flex justify-center mt-12 mb-20'>
                 <p className=' font-subtext max-w-3xl text-lg items-center text-center lg:text-2xl text-sponsor_light_orange leading-loose'>
-                Applications are currently OPEN for CruzHacks 2025! Our hackathon is open to hackers regardless of experience. Submit your application now!                </p>
+                Thank you for applying to CruzHacks 2025! Please be patient while we review applications. Stay tuned on your applicant portal and on our socials
+                for updates. We are excited to see you all in April!
+                 </p>
               </div>
 
               <div className="flex justify-center w-full gap-12 mb-12 md:mb-0">
                 <img src={Starfish1} alt="Starfish1" className="w-1/5 -mt-20 hidden md:flex"/>
-                <Link to='/apply'>
+                <Link to='/portal/applicant'>
                   <div className="bg-medium_yellow/65 px-4 rounded-full hover:bg-medium_yellow/85 text-center max-w-2xl">
-                    <p className="text-[#FFFFFF] px-8 py-4 text-lg md:text-xl md:px-8 md:py-6"><b>APPLY HERE</b></p>
+                    <p className="text-[#FFFFFF] px-8 py-4 text-lg md:text-xl md:px-8 md:py-6"><b>Log In to My Portal</b></p>
                   </div>
                 </Link>
                 <img src={Starfish2} alt="Starfish2" className="w-1/5 -mt-20 hidden md:flex"/>
               </div>
 
-              <div className='flex justify-center mb-12'>
-                <p className=' font-subtext max-w-3xl text-lg items-center text-center lg:text-2xl text-dark_pink leading-loose'>
-                  <b>UPDATE: Applications are open until Saturday March 8th @ 11:59PM! </b> </p>
-              </div>
               <div className="flex justify-center mb-12">
                 <p className="font-subtext max-w-3xl text-lg items-center text-center lg:text-xl text-sponsor_light_orange leading-loose">
                   if you have any questions about your application, please email <b>contact@cruzhacks.com</b></p>
