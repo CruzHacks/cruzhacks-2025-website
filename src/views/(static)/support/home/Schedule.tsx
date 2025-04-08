8177ce0e2c1d8ed3a8ce846f13806012fe5f4370
Sophie Han <32656696+sophiiehan@users.noreply.github.com>
2025-04-08 15:38:02 -0700

    Updated Schedule on support page

--- src/views/(static)/support/home/Schedule.tsx
+++ src/views/(static)/support/home/Schedule.tsx
@@ -4,32 +4,34 @@ import Card from "../../../../components/Card"
 
 const scheduleInfo = [
   {
-    date: "Friday, January 19th",
+    date: "Friday, April 11th",
     events: [
-      ["Hacker Registration/ Dinner", "5:00 pm"],
-      ["Opening Ceremony", "7:00 pm"],
-      ["Hacking / Workshops Begin", "9:00 pm"],
+      ["Event Begins. Start of registration/check-in for all participants", "4:00PM"],
+      ["Opening Ceremony", "6:00PM - 8:00PM"],
+      ["Hacker Registration/ Dinner", "8:00PM - 9:30PM"],
+      ["Ongoing Workshops", "8:00PM - 9:00PM"],
+      ["Hacking Begins!", "9:00 pm"],
     ],
   },
   {
-    date: "Saturday, January 20th",
+    date: "Saturday, April 12th",
     events: [
-      ["Breakfast", "9:00 am"],
-      ["Workshops Continue", "9:00 am"],
-      ["Lunch", "1:00 pm"],
-      ["Dinner", "6:00 pm"],
-      ["DevPost Soft Deadline", "10:00 pm"],
+      ["Breakfast", "8:00AM - 9:30AM"],
+      ["Workshops Continue", "9:00AM - 5:00PM"],
+      ["Lunch", "12:00PM - 1:30PM"],
+      ["Dinner", "6:00PM - 7:30PM"],
     ],
   },
   {
-    date: "Sunday, January 21st",
+    date: "Sunday, April 13th",
     events: [
-      ["Breakfast", "9:00 am"],
-      ["Code Freeze/ Submit Project", "9:00 am"],
-      ["Judging Begins", "11:30 am"],
-      ["Lunch", "2:00 pm"],
-      ["Closing Ceremony", "2:30 pm"],
-      ["CruzHacks 2024 Concludes", "4:00 pm"],
+      ["Devpost Soft Deadline", "7:30AM"],
+      ["Breakfast", "9:00AM"],
+      ["Code Freeze/ Submit Project", "9:00AM"],
+      ["Judging Begins", "11:30AM"],
+      ["Lunch", "12:30PM - 2:00PM"],
+      ["Closing Ceremony", "2:15PM"],
+      ["CruzHacks 2025 Concludes", "4:00PM"],
     ],
   },
 ]
@@ -43,14 +45,14 @@ const Schedule: React.FC = () => {
         <h2 className='text-center font-title text-2xl font-bold uppercase text-off_white md:text-3xl'>
           Schedule
         </h2>
-        {/* <h3 className='text-center text-off_white font-subtext'>{scheduleInfo[day].date}</h3> */}
+        <h3 className='text-center text-off_white font-subtext'>{scheduleInfo[day].date}</h3>
       </div>
 
-      <div>
+      {/* <div>
         <p className="text-center text-off_white font-subtext">We&apos;re actively working on finalizing logistics to ensure the best possible experience for you. Stay tuned for updates coming soon!</p>
-      </div>
+      </div> */}
 
-      {/* <div className='flex flex-col gap-10'>
+      <div className='flex flex-col gap-10'>
         <div className='flex justify-center'>
           {scheduleInfo.map((schedule, i) => {
             return (
@@ -98,7 +100,7 @@ const Schedule: React.FC = () => {
             })}
           </ul>
         </ul>
-      </div> */} 
+      </div> 
     </Card>
   )
 }
