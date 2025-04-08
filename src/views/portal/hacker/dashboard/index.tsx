731c9e3c8c64d3247ad54bcbd7ea4e080a8dc2d7
Sophie Han <32656696+sophiiehan@users.noreply.github.com>
2025-04-08 13:00:25 -0700

    Updated 2025 discord server link

--- src/views/portal/hacker/dashboard/index.tsx
+++ src/views/portal/hacker/dashboard/index.tsx
@@ -28,7 +28,7 @@ const DashbaordHacker = () => {
             help addmitted hackers team up and get help from mentors.
           </p>
           <a
-            href='https://discord.gg/2246PEzV'
+            href='https://discord.gg/XgJGBTT82R' //2025 discord server link
             target='_blank'
             rel='noreferrer'
             className='flex h-16 w-full items-center justify-center rounded-md bg-white px-3 py-1.5 text-center font-subtext text-lg leading-6 text-dark_orange shadow-sm transition-colors hover:bg-white/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-dark_orange disabled:bg-white/50 md:text-2xl'
